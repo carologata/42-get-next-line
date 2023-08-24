42e1b3cca71d078b2f288dc852676ac3deffff2e
Caroline Ogata <cogata@c3r12p5.42sp.org.br>
2023-08-24 14:13:17 -0300

    added bonus - passed tests and norminette

--- get_next_line_bonus.h
+++ get_next_line_bonus.h
@@ -1,17 +1,17 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   get_next_line.h                                    :+:      :+:    :+:   */
+/*   get_next_line_bonus.h                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: cogata <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/08/23 14:12:18 by cogata            #+#    #+#             */
-/*   Updated: 2023/08/23 14:12:21 by cogata           ###   ########.fr       */
+/*   Created: 2023/08/24 13:55:22 by cogata            #+#    #+#             */
+/*   Updated: 2023/08/24 13:55:23 by cogata           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef GET_NEXT_LINE_H
-# define GET_NEXT_LINE_H
+#ifndef GET_NEXT_LINE_BONUS_H
+# define GET_NEXT_LINE_BONUS_H
 
 # include <stdlib.h>
 # include <unistd.h>
